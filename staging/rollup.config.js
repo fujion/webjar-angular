518f3afd6cb8c26a5534972dfb55416eb60bbe70
Douglas Martin <mdgeek1@gmail.com>
2022-01-03 13:22:59 -0500

    Update build to generate UMD bundles for Angular 13+.

---
+++ staging/rollup.config.js
@@ -0,0 +1,226 @@
+export default [
+    {
+        input: './node_modules/@angular/animations/fesm2015/animations.mjs',
+        output: {
+            file: './dist/animations.umd.js',
+            format: 'umd',
+            name: 'ng.animations'
+        }
+    },
+    {
+        input: './node_modules/@angular/animations/fesm2015/browser.mjs',
+        output: {
+            file: './dist/animations-browser.umd.js',
+            format: 'umd',
+            name: 'ng.animationsBrowser'
+        }
+    },
+    {
+        input: './node_modules/@angular/animations/fesm2015/browser/testing.mjs',
+        output: {
+            file: './dist/animations-browser-testing.umd.js',
+            format: 'umd',
+            name: 'ng.animationsBrowserTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/common/fesm2015/common.mjs',
+        output: {
+            file: './dist/common.umd.js',
+            format: 'umd',
+            name: 'ng.common'
+        }
+    },
+    {
+        input: './node_modules/@angular/common/fesm2015/http.mjs',
+        output: {
+            file: './dist/common-http.umd.js',
+            format: 'umd',
+            name: 'ng.commonHttp'
+        }
+    },
+    {
+        input: './node_modules/@angular/common/fesm2015/testing.mjs',
+        output: {
+            file: './dist/common-testing.umd.js',
+            format: 'umd',
+            name: 'ng.commonTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/common/fesm2015/upgrade.mjs',
+        output: {
+            file: './dist/common-upgrade.umd.js',
+            format: 'umd',
+            name: 'ng.commonUpgrade'
+        }
+    },
+    {
+        input: './node_modules/@angular/common/fesm2015/http/testing.mjs',
+        output: {
+            file: './dist/common-http-testing.umd.js',
+            format: 'umd',
+            name: 'ng.commonHttpTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/compiler/fesm2015/compiler.mjs',
+        output: {
+            file: './dist/compiler.umd.js',
+            format: 'umd',
+            name: 'ng.compiler'
+        }
+    },
+    {
+        input: './node_modules/@angular/compiler/fesm2015/testing.mjs',
+        output: {
+            file: './dist/compiler-testing.umd.js',
+            format: 'umd',
+            name: 'ng.compilerTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/core/fesm2015/core.mjs',
+        output: {
+            file: './dist/core.umd.js',
+            format: 'umd',
+            name: 'ng.core'
+        }
+    },
+    {
+        input: './node_modules/@angular/core/fesm2015/testing.mjs',
+        output: {
+            file: './dist/core-testing.umd.js',
+            format: 'umd',
+            name: 'ng.coreTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/elements/fesm2015/elements.mjs',
+        output: {
+            file: './dist/elements.umd.js',
+            format: 'umd',
+            name: 'ng.elements'
+        }
+    },
+    {
+        input: './node_modules/@angular/forms/fesm2015/forms.mjs',
+        output: {
+            file: './dist/forms.umd.js',
+            format: 'umd',
+            name: 'ng.forms'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-browser/fesm2015/platform-browser.mjs',
+        output: {
+            file: './dist/platform-browser.umd.js',
+            format: 'umd',
+            name: 'ng.platformBrowser'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-browser/fesm2015/animations.mjs',
+        output: {
+            file: './dist/platform-browser-animations.umd.js',
+            format: 'umd',
+            name: 'ng.platformBrowserAnimations'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-browser/fesm2015/testing.mjs',
+        output: {
+            file: './dist/platform-browser-testing.umd.js',
+            format: 'umd',
+            name: 'ng.platformBrowserTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-browser-dynamic/fesm2015/platform-browser-dynamic.mjs',
+        output: {
+            file: './dist/platform-browser-dynamic.umd.js',
+            format: 'umd',
+            name: 'ng.platformBrowserDynamic'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-browser-dynamic/fesm2015/testing.mjs',
+        output: {
+            file: './dist/platform-browser-dynamic-testing.umd.js',
+            format: 'umd',
+            name: 'ng.platformBrowserDynamicTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-server/fesm2015/platform-server.mjs',
+        output: {
+            file: './dist/platform-server.umd.js',
+            format: 'umd',
+            name: 'ng.platformServer'
+        }
+    },
+    {
+        input: './node_modules/@angular/platform-server/fesm2015/testing.mjs',
+        output: {
+            file: './dist/platform-server-testing.umd.js',
+            format: 'umd',
+            name: 'ng.platformServerTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/router/fesm2015/router.mjs',
+        output: {
+            file: './dist/router.umd.js',
+            format: 'umd',
+            name: 'ng.router'
+        }
+    },
+    {
+        input: './node_modules/@angular/router/fesm2015/testing.mjs',
+        output: {
+            file: './dist/router-testing.umd.js',
+            format: 'umd',
+            name: 'ng.routerTesting'
+        }
+    },
+    {
+        input: './node_modules/@angular/router/fesm2015/upgrade.mjs',
+        output: {
+            file: './dist/router-upgrade.umd.js',
+            format: 'umd',
+            name: 'ng.routerUpgrade'
+        }
+    },
+    {
+        input: './node_modules/@angular/service-worker/fesm2015/service-worker.mjs',
+        output: {
+            file: './dist/service-worker.umd.js',
+            format: 'umd',
+            name: 'ng.serviceWorker'
+        }
+    },
+    {
+        input: './node_modules/@angular/service-worker/fesm2015/config.mjs',
+        output: {
+            file: './dist/service-worker-config.umd.js',
+            format: 'umd',
+            name: 'ng.serviceWorkerConfig'
+        }
+    },
+    {
+        input: './node_modules/@angular/upgrade/fesm2015/upgrade.mjs',
+        output: {
+            file: './dist/upgrade.umd.js',
+            format: 'umd',
+            name: 'ng.Upgrade'
+        }
+    },
+    {
+        input: './node_modules/@angular/upgrade/fesm2015/static.mjs',
+        output: {
+            file: './dist/upgrade-static.umd.js',
+            format: 'umd',
+            name: 'ng.UpgradeStatic'
+        }
+    }
+];
